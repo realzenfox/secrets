d860d3ee7890f62bf56e68a935f8e2440e034e77
govindforwork <42733570+govindforwork@users.noreply.github.com>
2022-05-12 17:29:20 +0530

    Added bcrypt for hashing and salting

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const app = express();
 
@@ -22,7 +25,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ['password'] });
+
 
 const User = new mongoose.model("User", userSchema);
 
@@ -41,18 +44,25 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
 
-  const newUser = new User({
-    email : req.body.username,
-    password : req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    const newUser = new User({
+      email : req.body.username,
+      password : hash
+      
+    });
+
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
+
   });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
 
 });
 
@@ -67,9 +77,14 @@ app.post("/login", function(req, res){
 
           if(foundUser){
 
-                  if(foundUser.password === password) {
-                    res.render("secrets");
-                  }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+
+                    if(result === true) {
+                      res.render("secrets");
+                    }
+
+                });
+
               }
         }
   });
